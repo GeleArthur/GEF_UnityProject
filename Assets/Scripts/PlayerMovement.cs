a249648bba88f5da0bdd9bdb017ccbcb8a9e1aa9
GeleArthur <geleminecrafter@gmail.com>
2024-10-19 23:49:59 +0200

    Started at rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask notPlayerMask;
     [Range(0,1)] [SerializeField] private float friction = 0.25f;
     [SerializeField] private float speed = 0.25f;
-    [SerializeField] private float airSpeed = 0.25f;
     
     private PlayerBodyManagement _bodyManagement;
     private Camera _playerCamera;
@@ -22,9 +21,9 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _playerRigidBody;
     private Vector2 _inputVector;
     
-    private bool _isGrounded = false;
+    private bool _isGrounded;
 
-    private Vector3 _rotationGoal = Vector3.zero;
+    public Vector3 _rotationGoal = Vector3.zero;
     
     private void Start()
     {
@@ -62,12 +61,25 @@ public class PlayerMovement : MonoBehaviour
         
         Rotation();
     }
+    
+    private Vector3[] _cornerOffset =
+    {
+        new(1, 0, 0),
+        new(-1, 0, 0),
+        new(0, 1, 0),
+        new(0, -1, 0),
+        new(0, 0, 1),
+        new(0, 0, -1),
+    };
 
     // TODO: Dont self intersect
     private void GroundCheck()
     {
         foreach (GameObject bodyPart in _bodyManagement.BodyParts)
         {
+            
+            
+            
             Debug.DrawRay(bodyPart.transform.position, Vector3.down * 1.0f, Color.red);
             if (Physics.Raycast(bodyPart.transform.position, Vector3.down, 1.0f, ~notPlayerMask, QueryTriggerInteraction.Ignore))
             {
@@ -104,12 +116,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void StartRotation(Vector2 input)
     {
-        _rotationGoal = new Vector3(0, input.x, 0);
+        
+        
+        _rotationGoal = new Vector3(MathF.Floor(input.x), 0, 0);
     }
 
     private void Rotation()
     {
-        _playerRigidBody.angularVelocity = Vector3.RotateTowards(_playerRigidBody.rotation.eulerAngles, _rotationGoal, 10 * Time.deltaTime, 0.0f);
+        Debug.DrawRay(transform.position, _rotationGoal * 3.0f, Color.red);
+        _rotationGoal.Normalize();
+        Quaternion deltaRotation = Quaternion.LookRotation(_rotationGoal) * Quaternion.Inverse(_playerRigidBody.rotation);
+        deltaRotation.ToAngleAxis(out float angle, out Vector3 axis);
+        
+        _playerRigidBody.angularVelocity = (axis * angle * Mathf.Deg2Rad) / Time.fixedDeltaTime;
     }
     
 }
